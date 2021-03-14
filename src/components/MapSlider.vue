101dd0a4fa7c95b3c90461e082258c5591d349cc
Alexander Golyshev <net.fallow@gmail.com>
2021-03-15 02:47:11 +0300

    Initial Commit

---
+++ src/components/MapSlider.vue
@@ -0,0 +1,88 @@
+<template>
+  <swiper class="swiper" :options="swiperOption">
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="map-page__interface-item">
+        <h2>Пенза</h2>
+        <p>1-10 сентября</p>
+      </div>
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+<style>
+  .swiper-slide {margin-right: -70px;}
+</style>
+<script>
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/swiper-bundle.css'
+
+  export default {
+    name: 'swiper-example-multiple-slides-per-biew',
+    title: 'Multiple slides per view',
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    data() {
+      return {
+        swiperOption: {
+          slidesPerView: 6,
+          spaceBetween: 10,
+          mousewheel: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          }
+        }
+      }
+    }
+  }
+</script>
